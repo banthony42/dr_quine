169246d67c06fa68ff6f1b63409f22114a5ecccd
banthony42 <banthony@student.42.fr>
2019-10-31 18:16:54 +0100

    [C] Wip Sully

--- C/Sully.c
+++ C/Sully.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
 #include<fcntl.h>
 #define OPEN(s) open(s,O_RDWR|O_CREAT)
-int main(){int n=5;char *a="";dprintf();}
+int main(){
+	int n=5;
+	if (n <= 0)return(0);
+	char buf[64]={0};
+	sprintf(buf, "Sully_%d.c", n);
+	OPEN(buf);
+}
