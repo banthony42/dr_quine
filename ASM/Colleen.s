8ac10c6426c752ad0292dd9ce76b841f5f8f4c4f
banthony42 <banthony@student.42.fr>
2019-11-04 12:06:05 +0100

    [ASM] Use lea instruction for string

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,6 +1,6 @@
 ;First comment
 section .data
-str:db"%1$cFirst comment%2$csection .data%2$cstr:db%3$c%4$s%3$c, 0%2$csection .text%2$cglobal _main%2$cglobal _func%2$cextern _printf%2$c_func:%2$cret%2$c_main:%2$c%1$cSecond comment%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$cmov rdi, str%2$cmov rsi, 59%2$cmov rdx, 10%2$cmov rcx, 34%2$cmov r8, str%2$ccall _printf%2$ccall _func%2$cleave%2$cret%2$c", 0
+str:db"%1$cFirst comment%2$csection .data%2$cstr:db%3$c%4$s%3$c, 0%2$csection .text%2$cglobal _main%2$cglobal _func%2$cextern _printf%2$c_func:%2$cret%2$c_main:%2$c%1$cSecond comment%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$clea rdi, [rel str]%2$cmov rsi, 59%2$cmov rdx, 10%2$cmov rcx, 34%2$cmov r8, str%2$ccall _printf%2$ccall _func%2$cleave%2$cret%2$c", 0
 section .text
 global _main
 global _func
@@ -12,7 +12,7 @@ _main:
 push rbp
 mov rbp, rsp
 sub rsp, 16
-mov rdi, str
+lea rdi, [rel str]
 mov rsi, 59
 mov rdx, 10
 mov rcx, 34
--- ASM/Grace.s
+++ ASM/Grace.s
@@ -6,7 +6,7 @@
 %macro MAIN_ROUTINE 0
 section .data
 file:db"Grace_kid.s", 0
-str:db"%1$cFirst comment%2$c%%define USELESS1 1%2$c%%define USELESS2 2%2$c%%define MAIN MAIN_ROUTINE%2$c%2$c%%macro MAIN_ROUTINE 0%2$csection .data%2$cfile:db%3$cGrace_kid.s%3$c, 0%2$cstr:db%3$c%4$s%3$c, 0%2$csection .text%2$cglobal _main%2$cextern _dprintf%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$cmov rax, 0x2000005%2$cmov rdi, file%2$cmov rsi, 0x0201%2$cmov rdx, 0777%2$csyscall%2$cmov rdi, rax%2$cmov rsi, str%2$cmov rdx, 59%2$cmov rcx, 10%2$cmov r8, 34%2$cmov r9, str%2$ccall _dprintf%2$cleave%2$cret%2$c%%endmacro%2$c%2$cMAIN", 0
+str:db"%1$cFirst comment%2$c%%define USELESS1 1%2$c%%define USELESS2 2%2$c%%define MAIN MAIN_ROUTINE%2$c%2$c%%macro MAIN_ROUTINE 0%2$csection .data%2$cfile:db%3$cGrace_kid.s%3$c, 0%2$cstr:db%3$c%4$s%3$c, 0%2$csection .text%2$cglobal _main%2$cextern _dprintf%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 16%2$cmov rax, 0x2000005%2$clea rdi, [rel file]%2$cmov rsi, 0x0201%2$cmov rdx, 0777%2$csyscall%2$cmov rdi, rax%2$clea rsi, [rel str]%2$cmov rdx, 59%2$cmov rcx, 10%2$cmov r8, 34%2$clea r9, [rel str]%2$ccall _dprintf%2$cleave%2$cret%2$c%%endmacro%2$c%2$cMAIN", 0
 section .text
 global _main
 extern _dprintf
@@ -15,16 +15,16 @@ push rbp
 mov rbp, rsp
 sub rsp, 16
 mov rax, 0x2000005
-mov rdi, file
+lea rdi, [rel file]
 mov rsi, 0x0201
 mov rdx, 0777
 syscall
 mov rdi, rax
-mov rsi, str
+lea rsi, [rel str]
 mov rdx, 59
 mov rcx, 10
 mov r8, 34
-mov r9, str
+lea r9, [rel str]
 call _dprintf
 leave
 ret
