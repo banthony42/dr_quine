104062eff448d1ad9d6edee0491227cd0961db3f
banthony42 <banthony@student.42.fr>
2019-11-18 13:33:16 +0100

    [ASM] Fix bad stop condition

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -2,7 +2,7 @@ section .data
 max:dq 5
 file_prefix:db"Sully_%d.s", 0
 cmd:db"nasm -f macho64 -DOSX Sully_%1$d.s && ld Sully_%1$d.o -macosx_version_min 10.8 -lSystem -o Sully_%1$d && rm Sully_*.o && ./Sully_%1$d", 0
-str:db"section .data%1$cmax:dq %4$d%1$cfile_prefix:db%2$cSully_%%d.s%2$c, 0%1$ccmd:db%2$cnasm -f macho64 -DOSX Sully_%%1$d.s && ld Sully_%%1$d.o -macosx_version_min 10.8 -lSystem -o Sully_%%1$d && rm Sully_*.o && ./Sully_%%1$d%2$c, 0%1$cstr:db%2$c%3$s%2$c, 0%1$csection .bss%1$cfile_buf resb 64%1$cbuf resb 255%1$csection .text%1$cglobal _main%1$cextern _printf%1$cextern _dprintf%1$cextern _sprintf%1$cextern _access%1$cextern _system%1$c%1$c_main:%1$cpush rbp%1$cmov rbp, rsp%1$cmov r15, [rel max]%1$c;Stop condition%1$ccmp r15, 0%1$cje .exit%1$c;Build file name%1$clea rdi, [rel file_buf]%1$clea rsi, [rel file_prefix]%1$cmov rdx, r15%1$ccall _sprintf%1$c;Check file exist%1$clea rdi, [rel file_buf]%1$cmov rsi, 0%1$ccall _access%1$c;Decrease%1$ccmp rax, -1%1$cje .endif%1$csub r15, 1%1$c.endif:%1$c;Rebuild file name%1$clea rdi, [rel file_buf]%1$clea rsi, [rel file_prefix]%1$cmov rdx, r15%1$ccall _sprintf%1$c;Create file%1$cmov rax, 0x2000005%1$clea rdi, [rel file_buf]%1$cmov rsi, 0x0201%1$cmov rdx, 0777%1$csyscall%1$ccmp rax, -1%1$cje .exit%1$c;Write prog into it%1$cmov rdi, rax%1$clea rsi, [rel str]%1$cmov rdx, 10%1$cmov rcx, 34%1$clea r8, [rel str]%1$cmov r9, r15%1$ccall _dprintf%1$c;Compile and run%1$clea rdi, [rel buf]%1$clea rsi, [rel cmd]%1$cmov rdx, r15%1$ccall _sprintf%1$clea rdi, [rel buf]%1$ccall _system%1$c.exit:%1$cleave%1$cret", 0
+str:db"section .data%1$cmax:dq %4$d%1$cfile_prefix:db%2$cSully_%%d.s%2$c, 0%1$ccmd:db%2$cnasm -f macho64 -DOSX Sully_%%1$d.s && ld Sully_%%1$d.o -macosx_version_min 10.8 -lSystem -o Sully_%%1$d && rm Sully_*.o && ./Sully_%%1$d%2$c, 0%1$cstr:db%2$c%3$s%2$c, 0%1$csection .bss%1$cfile_buf resb 64%1$cbuf resb 255%1$csection .text%1$cglobal _main%1$cextern _printf%1$cextern _dprintf%1$cextern _sprintf%1$cextern _access%1$cextern _system%1$c%1$c_main:%1$cpush rbp%1$cmov rbp, rsp%1$cmov r15, [rel max]%1$c;Stop condition%1$ccmp r15, 0%1$cjng .exit%1$c;Build file name%1$clea rdi, [rel file_buf]%1$clea rsi, [rel file_prefix]%1$cmov rdx, r15%1$ccall _sprintf%1$c;Check file exist%1$clea rdi, [rel file_buf]%1$cmov rsi, 0%1$ccall _access%1$c;Decrease%1$ccmp rax, -1%1$cje .endif%1$csub r15, 1%1$c.endif:%1$c;Rebuild file name%1$clea rdi, [rel file_buf]%1$clea rsi, [rel file_prefix]%1$cmov rdx, r15%1$ccall _sprintf%1$c;Create file%1$cmov rax, 0x2000005%1$clea rdi, [rel file_buf]%1$cmov rsi, 0x0201%1$cmov rdx, 0777%1$csyscall%1$ccmp rax, -1%1$cje .exit%1$c;Write prog into it%1$cmov rdi, rax%1$clea rsi, [rel str]%1$cmov rdx, 10%1$cmov rcx, 34%1$clea r8, [rel str]%1$cmov r9, r15%1$ccall _dprintf%1$c;Compile and run%1$clea rdi, [rel buf]%1$clea rsi, [rel cmd]%1$cmov rdx, r15%1$ccall _sprintf%1$clea rdi, [rel buf]%1$ccall _system%1$c.exit:%1$cleave%1$cret", 0
 section .bss
 file_buf resb 64
 buf resb 255
@@ -20,7 +20,7 @@ mov rbp, rsp
 mov r15, [rel max]
 ;Stop condition
 cmp r15, 0
-je .exit
+jng .exit
 ;Build file name
 lea rdi, [rel file_buf]
 lea rsi, [rel file_prefix]
